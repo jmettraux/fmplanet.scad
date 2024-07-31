5c82103c1029e5077b3a909cf64e07451ecc17db
John Mettraux <jmettraux@gmail.com>
2024-08-01 08:31:23 +0900

    Display hex keys in .scad

--- lib/tile_scad.rb
+++ lib/tile_scad.rb
@@ -62,6 +62,23 @@ class Tile
     s << File.read('lib/head.scad')
     s << "\n\n// " << "-" * 78
     s << "\n"
+
+    s << "\n" << %{
+//        a0  b0  c0  d0
+//
+//      a1  b1  c1  d1  e1
+//
+//    a2  b2  c2  d2  e2  f2
+//
+//  a3  b3  c3  d3  e3  f3  g3
+//
+//    b4  c4  d4  e4  f4  g4
+//
+//      c5  d5  e5  f5  g5
+//
+//        d6  e6  f6  g6
+      }.strip << "\n\n"
+
     s << "/*" << to_s.rstrip << "\n*/\n\n"
 
     s << swamp_cover_scad
